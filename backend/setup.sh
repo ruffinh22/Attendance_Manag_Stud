82baa3f1e113b9b64c1c4fe8ff2296d9dc39590e
ruffinh22 <110920067+ruffinh22@users.noreply.github.com>
2024-06-18 07:03:35 +0100

    Update setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -10,12 +10,12 @@ check_error() {
 
 # Mettre à jour les paquets et installer Python et pip
 echo "Mise à jour des paquets et installation de Python et pip..."
-sudo apt update && sudo apt install -y python3 python3-pip
+apt update && apt install -y python3 python3-pip
 check_error
 
 # Installer les dépendances pour mysqlclient
 echo "Installation des dépendances pour mysqlclient..."
-sudo apt install -y python3-dev default-libmysqlclient-dev build-essential
+apt install -y python3-dev default-libmysqlclient-dev build-essential
 check_error
 
 # Installer mysqlclient
@@ -25,17 +25,17 @@ check_error
 
 # Installer MySQL
 echo "Installation de MySQL..."
-sudo apt install -y mysql-server
+apt install -y mysql-server
 check_error
 
 # Sécuriser l'installation de MySQL
 echo "Sécurisation de MySQL..."
-sudo mysql_secure_installation
+mysql_secure_installation
 check_error
 
 # Créer la base de données et l'utilisateur MySQL
 echo "Configuration de MySQL..."
-sudo mysql -u root -p <<MYSQL_SCRIPT
+mysql -u root -p <<MYSQL_SCRIPT
 CREATE DATABASE attendance;
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'Admin123123@';
 GRANT ALL PRIVILEGES ON attendance.* TO 'admin'@'localhost';
@@ -45,27 +45,27 @@ check_error
 
 # Vérifier et ajuster les permissions du socket MySQL
 echo "Vérification des permissions du socket MySQL..."
-sudo chown mysql:mysql /var/run/mysqld/mysqld.sock
-sudo chmod 777 /var/run/mysqld/mysqld.sock
+chown mysql:mysql /var/run/mysqld/mysqld.sock
+chmod 777 /var/run/mysqld/mysqld.sock
 check_error
 
 # Redémarrer MySQL pour appliquer les changements
 echo "Redémarrage de MySQL..."
-sudo systemctl restart mysql
+systemctl restart mysql
 check_error
 
 # Installer MongoDB
 echo "Installation de MongoDB..."
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt update
-sudo apt install -y mongodb-org
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt update
+apt install -y mongodb-org
 check_error
 
 # Démarrer MongoDB
 echo "Démarrage de MongoDB..."
-sudo systemctl start mongod
-sudo systemctl enable mongod
+systemctl start mongod
+systemctl enable mongod
 check_error
 
 # Configurer MongoDB

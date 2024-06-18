f44a54a36923011b78d552f17eba39066448558c
ruffinh22 <110920067+ruffinh22@users.noreply.github.com>
2024-06-18 02:50:55 +0100

    Update setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Mise à jour du système.."
+
+sudo apt install python3-dev default-libmysqlclient-dev build-essential
+pip install mysqlclient
 
 # Mettre à jour le système
 echo "Mise à jour du système..."

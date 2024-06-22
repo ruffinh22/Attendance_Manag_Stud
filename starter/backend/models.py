23782ecb4b966542f35ecaf5a5a7a9a4893862d3
ruffin hounsounnon <ruffinhounsounnon@gmail.com>
2024-06-22 13:57:10 +0100

    update trivia quizze projectto create db

--- starter/backend/models.py
+++ starter/backend/models.py
@@ -124,4 +124,4 @@ def bad_request(error):
 
 # Ensure app.run() is called to start the Flask application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
